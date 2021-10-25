802f7cd912cd52c1ba9455a1dc483b7055c90076
daqger <daggerempire@gmail.com>
2021-10-24 21:58:10 -0400

    parser complete

--- Expression.java
+++ Expression.java
@@ -3,9 +3,16 @@ import java.util.List;
 abstract class Expression {
   interface Visitor<E> {
     E visitAssignExpr(Assign expr);
+    E visitVariableExpr(Variable expr);
+    E visitBinaryExpr(Binary expr);
+    E visitUnaryExpr(Unary expr);
+    E visitLogicalExpr(Logical expr);
+    E visitLiteralExpr(Literal expr);
+    E visitCallExpr(Call expr);
+    E visitGetExpr(Get expr);
+    E visitSetExpr(Set expr);
   }
-
-
+////////////////////////////////////////////////////////////////////////////////////////////
   static class Assign extends Expression{
     Assign(Token name, Expression value) {
       this.name = name;
@@ -19,8 +26,114 @@ abstract class Expression {
     final Token name;
     final Expression value;
   }
-
-
-
-  abstract <E> E accept(Visitor<E> visitor);
+////////////////////////////////////////////////////////////////////////////////////////////
+  static class Variable extends Expression{
+    Variable(Token name) {
+      this.name = name;
+    }
+    @Override
+    <E> E accept(Visitor<E> visitor) {
+      return visitor.visitVariableExpr(this);
+    }
+    final Token name;
+  }
+////////////////////////////////////////////////////////////////////////////////////////////
+  static class Binary extends Expression{
+    Binary(Expression expr, Token operator, Expression right) {
+      this.expr = expr;
+      this.operator = operator;
+      this.right = right;
+    }
+    @Override
+    <E> E accept(Visitor<E> visitor) {
+      return visitor.visitBinaryExpr(this);
+    }
+    final Expression expr;
+    final Token operator;
+    final Expression right;
+  }
+////////////////////////////////////////////////////////////////////////////////////////////
+  static class Unary extends Expression{
+    Unary(Token operator, Expression expr) {
+      this.operator = operator;
+      this.expr = expr;
+    }
+    @Override
+    <E> E accept(Visitor<E> visitor) {
+      return visitor.visitUnaryExpr(this);
+    }
+    final Token operator;
+    final Expression expr;
+  }
+  ////////////////////////////////////////////////////////////////////////////////////////////
+  static class Logical extends Expression{
+    Logical(Expression expr,Token operator, Expression right){
+      this.expr = expr;
+      this.operator = operator;
+      this.right = right;
+    }
+    @Override
+    <E> E accept(Visitor<E> visitor) {
+      return visitor.visitLogicalExpr(this);
+    }
+    final Expression expr;
+    final Token operator;
+    final Expression right;
+  }
+  ////////////////////////////////////////////////////////////////////////////////////////////
+  static class Literal extends Expression{
+    Literal(Object value) {
+      this.value = value;
+    }
+    @Override
+    <E> E accept(Visitor<E> visitor) {
+      return visitor.visitLiteralExpr(this);
+    }
+    final Object value;
+  }
+  ////////////////////////////////////////////////////////////////////////////////////////////
+  static class Call extends Expression{
+    Call(Expression expr, Token paren, List<Expression> args) {
+      this.expr = expr;
+      this.paren = paren;
+      this.args = args;
+    }
+    @Override
+    <E> E accept(Visitor<E> visitor) {
+      return visitor.visitCallExpr(this);
+    }
+    final Expression expr;
+    final Token paren;
+    final List<Expression> args;
+  }
+////////////////////////////////////////////////////////////////////////////////////////////
+static class Get extends Expression{
+  Get(Expression object, Token name) {
+    this.object = object;
+    this.name = name;
+  }
+  @Override
+  <E> E accept(Visitor<E> visitor) {
+    return visitor.visitGetExpr(this);
+  }
+  final Expression object;
+  final Token name;
+}
+////////////////////////////////////////////////////////////////////////////////////////////
+static class Set extends Expression{
+  Set(Expression object, Token name, Expression value) {
+    this.object = object;
+    this.name = name;
+    this.value = value;
+  }
+  @Override
+  <E> E accept(Visitor<E> visitor) {
+    return visitor.visitSetExpr(this);
+  }
+  final Expression object;
+  final Token name;
+  final Expression value;
+}
+  
+abstract <E> E accept(Visitor<E> visitor);
 }
--- Statement.java
+++ Statement.java
@@ -2,13 +2,31 @@ import java.util.List;
 
 abstract class Statement{
   interface Visitor<S> {
+    S visitExprStmt(Expr stmt);
     S visitFunctionStmt(Function stmt);
+    S visitVariableStmt(Variable stmt);
+    S visitForStmt(For stmt);
+    S visitPrintStmt(Print stmt);
+    S visitPrintlnStmt(Println stmt);
+    S visitWhileStmt(While stmt);
+    S visitIfStmt(If stmt);
   }
+////////////////////////////////////////////////////////////////////////////////////////////
+  static class Expr extends Statement {
+    Expr(Expression expression) {
+      this.expression = expression;
+    }
 
+    @Override
+    <S> S accept(Visitor<S> visitor) {
+      return visitor.visitExprStmt(this);
+    }
+    final Expression expression;
+  }
+////////////////////////////////////////////////////////////////////////////////////////////
   static class Function extends Statement{
-    Function(Token name, List<Token> paramstype, List<Token> params, List<Statement> body) {
-      this.name = name;
-      this.paramstype = paramstype;
+    Function(Token name,List<Token> params, List<Statement> body) {
+      this.name = name;      
       this.params = params;
       this.body = body;
     }
@@ -18,16 +36,91 @@ abstract class Statement{
       return visitor.visitFunctionStmt(this);
     }
 
-    final Token name;
-    final List<Token> paramstype;
+    final Token name;    
     final List<Token> params;
     final List<Statement> body;
   }
+////////////////////////////////////////////////////////////////////////////////////////////
+  static class For extends Statement{
+    For(Token iterator, Token range, List<Statement> body) {
+      this.iterator = iterator;
+      this.range = range;
+      this.body = body;
+    }
 
+    @Override
+    <S> S accept(Visitor<S> visitor) {
+      return visitor.visitForStmt(this);
+    }
 
-
-
-
-
+    final Token iterator;
+    final Token range;
+    final List<Statement> body;
+  }
+////////////////////////////////////////////////////////////////////////////////////////////
+  static class If extends Statement{
+    If(Expression condition, Statement elseifbody, Statement elsebody) {
+      this.condition = condition;
+      this.elseifbody = elseifbody;
+      this.elsebody = elsebody;
+    }
+    @Override
+    <S> S accept(Visitor<S> visitor) {
+      return visitor.visitIfStmt(this);
+    }
+    final Expression condition;
+    final Statement elseifbody;
+    final Statement elsebody;
+  }
+////////////////////////////////////////////////////////////////////////////////////////////
+  static class While extends Statement{
+    While(Expression condition, Statement body){
+      this.condition = condition;
+      this.body = body;
+    }
+    @Override
+    <S> S accept(Visitor<S> visitor) {
+      return visitor.visitWhileStmt(this);
+    }
+    final Expression condition;
+    final Statement body;
+  }
+////////////////////////////////////////////////////////////////////////////////////////////
+  static class Print extends Statement{
+    Print(Expression text){
+      this.text = text;
+    }
+    @Override
+    <S> S accept(Visitor<S> visitor) {
+      return visitor.visitPrintStmt(this);
+    }
+    final Expression text;
+  }
+  ////////////////////////////////////////////////////////////////////////////////////////////
+  static class Println extends Statement{
+    Println(Expression text){
+      this.text = text;
+    }
+    @Override
+    <S> S accept(Visitor<S> visitor) {
+      return visitor.visitPrintlnStmt(this);      
+    }
+    final Expression text;
+  }
+  ////////////////////////////////////////////////////////////////////////////////////////////
+  static class Variable extends Statement{
+    Variable(Token name, Expression initializer) {
+      this.name = name;
+      this.initializer = initializer;
+    }
+    @Override
+    <S> S accept(Visitor<S> visitor) {
+      return visitor.visitVariableStmt(this);
+    }
+    final Token name;
+    final Expression initializer;
+  }
+  ////////////////////////////////////////////////////////////////////////////////////////////
+  
   abstract <S> S accept(Visitor<S> visitor);
 }
